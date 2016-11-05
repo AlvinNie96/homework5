d61dd93922a4e35ba7c3d8e325a588a732678bb3
alvinnie96 <alvinnie96@gmail.com>
2016-11-05 09:15:44 -0400

    hw5

---
+++ Alvin_homework5.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Ruin and Rising (2014)
+//Siege and Storm (2013)
+//Shadow and Bone (2012)
+//Crooked Kingdom (2016)
+//Six of Crows (2015)
+//Navigators of Dune (2016)
+//Brotherhood in Death (2016)
+//Devoted in Death (2015)
+//Into Everywhere (2016)
+//Guns of Empire, the (2016)
+//
+//
+//Create a database that includes the id number (1-10), title, publication year, and a checked_out field. Example:
+//[{"id":"1","title":"Ruin and Rising","publication_year": "2014", "checked_out": "false"}, ....]
+//
+
+var rar = ["title":"Ruin and Rising", "id":"1", "publication_year":"2014", "check_out":"false"]
+var sas = ["title":"Siege and Storm", "id":"2", "publication_year":"2013", "check_out":"false"]
+var sab = ["title":"Shadow and Bone", "id":"3", "publication_year":"2012", "check_out":"false"]
+var ck = ["title":"Crooked Kingdom", "id":"4", "publication_year":"2016", "check_out":"false"]
+var soc = ["title":"Six of Crows", "id":"5", "publication_year":"2015", "check_out":"false"]
+var nof = ["title":"Navigators of Dune", "id":"6", "publication_year":"2016", "check_out":"false"]
+var bod = ["title":"Brotherhood in Death", "id":"7", "publication_year":"2016", "check_out":"false"]
+var did = ["title":"Devoted in Death", "id":"8", "publication_year":"2015", "check_out":"false"]
+var ie = ["title":"Into Everywhere", "id":"9", "publication_year":"2016", "check_out":"false"]
+var goe = ["title":"Guns of Empire", "id":"10", "publication_year":"2015", "check_out":"false"]
+
+var books = [rar, sas, sab, ck, soc, nof, bod, did, ie, goe]
